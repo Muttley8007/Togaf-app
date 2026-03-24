ee1bbf6e03ededd804d0806d77c94964a0398eeb
Michael <msnajdar2@hotmail.com>
2026-03-24 18:46:15 +1100

    Update lesson1.js

--- lesson1.js
+++ lesson1.js
@@ -45,7 +45,7 @@ const lesson1 = {
     label: "Architecture Development Method (ADM)",
     url: "https://www.opengroup.org/togaf/adm"
   }
-]
+],
 
       detail: "TOGAF provides a method, a common vocabulary, and a way to think about architecture as an enterprise capability rather than just a one-off design exercise.",
       example: "If an organisation wants to modernise customer service, TOGAF helps make sure business goals, customer data, applications, and infrastructure are considered together instead of each team making separate decisions.",
