951a3f1e120b52fd6f9c0faabee2b93edff26976
Michael <msnajdar2@hotmail.com>
2026-03-24 19:32:51 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // TOGAF TOOL v1.0.0
 // app.js
 
-const lessons = [lesson1, lesson2];
+const lessons = [lesson1, lesson2, lesson3, lesson4, lesson5, lesson6, lesson7, lesson8, lesson9, lesson10];
 
 let activeLesson = null;
 let currentCard = 0;
